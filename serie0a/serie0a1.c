a09639708c6fdc6c05a60dbd4b5439b48ee02a9f
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 20:01:00 +0200

    serie0a1 indentada

--- serie0a/serie0a1.c
+++ serie0a/serie0a1.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-int
-main ()
-
+int main ()
 {
-  float F,C;
-  C=5;
-F= C*1.8 +32;
- printf("Temperatura em Fahrenheit: %f\n", F);
-
- return 0;
+	float F,C;
+	C=5;
+	F= C*1.8 +32;
+	printf("Temperatura em Fahrenheit: %f\n", F);
+	return 0;
 }
