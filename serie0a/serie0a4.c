5ce6d7bef27f9a1b3722dbfe8b8b3d6776da0614
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 20:27:01 +0200

    serie0a4 indentada

--- serie0a/serie0a4.c
+++ serie0a/serie0a4.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int
-main ()
-
+int main ()
 {
-  float F,C;
-  printf ("Que temperatura em graus celsius quer calcular?\n");
-  scanf ("%f",&C);
+	float F,C;
 
-F= C*1.8 +32;
- printf("Temperatura em Fahrenheit: %f\n", F);
+	printf ("Que temperatura em graus celsius quer calcular?\n");
+	scanf ("%f",&C);
+	F= C*1.8 +32;
 
- return 0;
+	printf("Temperatura em Fahrenheit: %f\n", F);
+	return 0;
 }
