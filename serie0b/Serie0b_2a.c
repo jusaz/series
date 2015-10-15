3e6200e6fea21db8fcf7736ff67160824ec72276
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 21:33:30 +0200

    codigo indentado serie0b2_a

--- serie0b/Serie0b_2a.c
+++ serie0b/Serie0b_2a.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int
-main ()
-
+int main ()
 {
-  float F,C;
-  printf ("Que temperatura em graus celsius quer calcular?\n");
-  scanf ("%f",&C);
+	float F,C;
 
-F= C*1.8 +32;
- printf("%f Celsius equivale a %f Fahrenheit\n", C, F);
+	printf ("Que temperatura em graus celsius quer calcular?\n");
+	scanf ("%f", &C);
+	F = C*1.8 + 32;
+	printf("%f Celsius equivale a %f Fahrenheit\n", C, F);
 
- return 0;
+	return 0;
 }
