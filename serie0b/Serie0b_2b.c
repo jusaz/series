d5f3b3c24998e7175e855f38ca58c7a21f4c301b
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 21:52:43 +0200

    codigo indentado serie0b2_b

--- serie0b/Serie0b_2b.c
+++ serie0b/Serie0b_2b.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 
-int
-main ()
-
+int main ()
 {
-  float F,C;
-  int E;
-  printf ("Escolha a conversão que quer efetuar: 1=C para F ou 2= F para C\n");
+    float F,C;
+    int E;
+    printf ("Escolha a conversão que quer efetuar: 1=C para F ou 2= F para C\n");
     scanf ("%d", &E);
-    if (E ==1)
-      {
-  printf ("Que temperatura em graus celsius quer calcular?\n");
-  scanf ("%f",&C);
 
+    if (E == 1)
+    {
+        printf ("Que temperatura em graus celsius quer calcular?\n");
+        scanf ("%f",&C);
+        F= C*1.8 +32;
+        printf("%f Celsius equivale a %f Fahrenheit\n", C, F);
+    }
 
-F= C*1.8 +32;
- printf("%f Celsius equivale a %f Fahrenheit\n", C, F);
+    if (E == 2)
+    {
+        printf ("Que temperatura em graus fahrenheit quer calcular?\n");
+        scanf ("%f",&F);
+        C=(F-32)/1.8;
+        printf("%f Fahrenheit equivale a %f Celsius\n", F, C);
+    }
 
-      }
-
-   if (E ==2)
-      {
-  printf ("Que temperatura em graus fahrenheit quer calcular?\n");
-  scanf ("%f",&F);
-
-  C=(F-32)/1.8;
-  
- printf("%f Fahrenheit equivale a %f Celsius\n", F, C);
-
-      }
-
- return 0;
+  return 0;
 }
