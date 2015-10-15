cd2ebf2f95a41c52d9880be0b615d19b42e1b151
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 20:22:51 +0200

    serie0a3 indentada e comentada

--- serie0a/serie0a3.c
+++ serie0a/serie0a3.c
@@ -4,17 +4,22 @@ int
 main ()
 
 {
-  float F;int C;
-  C=0;
-
-  while (C<=100)
+	// um conselho: não uses nomes de variáveis como este C, F.
+	// em programas mais complicados não sabes a que é que se refere
+	// e torna a leitura do código complicada
+	// Ficava melhor: float tempFahr, int tempCelcius 
+	// e nunca uses nomes de variáveis só em maiusculas.
+	// normalmente o nome todo em maisculas é só usado para #defines
+	// que sáo directivas usadas o pré processamente como o #include :)
+	float F;int C;  
+	C=0;
+	while (C<=100)
     {
-F= C*1.8 +32;
- 
- printf("%d Celsius =  %6.2f Fahrenheit\n", C, F);
- C=C+1.;
-
+    	F= C*1.8 +32;
+    	printf("%d Celsius =  %6.2f Fahrenheit\n", C, F);
+    	//não percebo a indentação por 1. usar C++
+    	C++; 
     }
 
- return 0;
+    return 0;
 }
