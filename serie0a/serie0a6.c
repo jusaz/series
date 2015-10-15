22523de91469f28aec82998755a3359a679b51ea
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 20:36:26 +0200

    serie0a6 indentada

--- serie0a/serie0a6.c
+++ serie0a/serie0a6.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
-int
-main()
-{
-  int x,y,z;
-  y=0;
 
-  printf ("Escolha a tabuada\n");
-  scanf ("%d", &x);
-  if(x<0||x>=50)
+int main()
+{
+    int x,y,z;
+    y=0;
+
+    printf ("Escolha a tabuada\n");
+    scanf ("%d", &x);
+  
+    if(x<0||x>=50)
     {
-      printf ("YOU WISH\n ");
-      return 0;
+        printf ("YOU WISH\n ");
+        return 0;
     }
 
     printf ("\n Tabuada do %d\n", x);
 
-  while (y<=10)
+    while (y<=10)
     { 
-z=x*y;
- 
- printf ("%d X %d = %d \n",x, y, z);
- y=y+1.;
+        z=x*y;
+        printf ("%d X %d = %d \n",x, y, z);
+        y=y+1.;
     }
-
-  return 0;
-    }
+    
+    return 0;
+}
