c04119279a989afb670c49e89e94650719d1c179
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-15 20:12:02 +0200

    serie0a2 indentada

--- serie0a/serie0a2.c
+++ serie0a/serie0a2.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int
-main()
+
+int main()
 {
-  int x,y,z;
-  x=9;
-  y=0;
-printf ("\n Tabuada do 9\n");
+	int x,y,z;
+	x=9;
+	y=0;
 
-  while (y<=10)
-    { 
-z=x*y;
- 
- printf ("%d X %d = %d \n",x, y, z);
- y=y+1.;
+	printf ("\n Tabuada do 9\n");
+	
+	while (y<=10)
+	{ 
+		z=x*y;
+		printf ("%d X %d = %d \n", x, y, z);
+		y=y+1.; //normalmente quando incrementas a variável em 1 escreves (e neste caso) y++ 
     }
 
-  return 0;
-    }
+    return 0;
+}
