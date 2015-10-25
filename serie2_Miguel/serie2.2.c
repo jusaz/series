d0f2449d1a7e6afcc0e7a729def503e15d9f28a6
jusaz <azevedo.miguel.jose@gmail.com>
2015-10-25 18:00:51 +0100

    serie 2 exercicio 2 indentado

--- serie2_Miguel/serie2.2.c
+++ serie2_Miguel/serie2.2.c
@@ -4,43 +4,43 @@
 
 double conversion (double x) 
 {
-	if (x<0) 
-	{ 
-		x = -x; 
-	}
+    if (x<0) 
+    { 
+        x = -x; 
+    }
 
-	x = ((double) fmod (x, 2*M_PI));
+    x = ((double) fmod (x, 2*M_PI));
 
-	return x; 
+    return x; 
 }
 
 
 double factorial (int mouse) 
 {
-	double mickey; 
-	mickey = 1;
-	while (mouse >1)
-	{
-		mickey = mickey * mouse; 
-		--mouse;
-	}
-	return mickey; 
+    double mickey; 
+    mickey = 1;
+    while (mouse >1)
+    {
+        mickey = mickey * mouse; 
+        --mouse;
+    }
+    return mickey; 
 }
 
 double cos2 (double valor)   
 {
-	double cos2, adicao, adicao1;
-	cos2=0;
-	int i;
-	i=0; 
-	while(1)
-	{
-		adicao= pow((-1),i)* (pow (valor, 2*i))/(factorial(2*i));
-		cos2 = cos2 + adicao; 
-		if (fabs(adicao) < 1.0e-10)
-			break; 
-			// printf ("%lf %lf\n", adicao, cos2);
-		++i;
+    double cos2, adicao, adicao1;
+    cos2=0;
+    int i;
+    i=0; 
+    while(1)
+    {
+        adicao= pow((-1),i)* (pow (valor, 2*i))/(factorial(2*i));
+        cos2 = cos2 + adicao; 
+        if (fabs(adicao) < 1.0e-10)
+            break; 
+            // printf ("%lf %lf\n", adicao, cos2);
+        ++i;
     }
 
     //printf ("%lf\n", cos2);
@@ -51,16 +51,16 @@ double cos2 (double valor)
 
 int main () 
 { 
-	int x;
-	double cosseno, valor;
+    int x;
+    double cosseno, valor;
 
-	printf ("Escolha o valor de x\n");
-	scanf ("%d", &x ); 
-	valor= conversion (x);
-	cosseno = cos2 (valor); 
+    printf ("Escolha o valor de x\n");
+    scanf ("%d", &x ); 
+    valor= conversion (x);
+    cosseno = cos2 (valor); 
 
-	printf ("O cosseno de '%d' é: %.10lf\n", x, cosseno);
-	//printf ("%lf\n", cos(valor));
+    printf ("O cosseno de '%d' é: %.10lf\n", x, cosseno);
+    //printf ("%lf\n", cos(valor));
 
-	return 0;
+    return 0;
 }
